5decce41836cc31b221c79aff2ffc23270233a2b
morfeuss <kikbika@gmail.com>
2025-02-25 09:39:59 +0300

    начало

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace alesya_rassylka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
