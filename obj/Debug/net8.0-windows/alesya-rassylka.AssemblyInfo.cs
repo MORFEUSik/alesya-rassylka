812fdf2eb9bc6a8c12429906cef61d9de0ab385d
morfeuss <kikbika@gmail.com>
2025-02-25 09:44:06 +0300

    добавил гитигнор

--- obj/Debug/net8.0-windows/alesya-rassylka.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/alesya-rassylka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alesya-rassylka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5decce41836cc31b221c79aff2ffc23270233a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("alesya-rassylka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alesya-rassylka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
