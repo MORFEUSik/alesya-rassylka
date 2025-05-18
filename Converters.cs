7bae730d0fdc2cedf84bb7e7ae854af136b34969
morfeus <kikbika@gmail.com>
2025-05-18 10:13:16 +0300

    кнопки справа

---
+++ Converters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace alesya_rassylka
+{
+    public class WidthMinusButtonsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double listBoxWidth && parameter is double buttonsWidth)
+            {
+                return listBoxWidth - buttonsWidth - 20; // Вычитаем ширину кнопок и отступы
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- obj/Debug/net8.0-windows/alesya-rassylka.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/alesya-rassylka.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alesya-rassylka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7133db98e7349d31d17a8df0dd19fba8d1fb581c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c82bae06d8a9c88285d40e38855a39eaeb37ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("alesya-rassylka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alesya-rassylka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
